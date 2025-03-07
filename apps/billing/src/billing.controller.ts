bac5aa8fa16bc2246aeb4e1d82dc7e89a3b74801
fabogit <fabo.dev.email@gmail.com>
2025-03-07 04:36:32 +0100

    #8
    billing rmq microservice

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { BillingService } from './billing.service';
 
 @Controller()
 export class BillingController {
   constructor(private readonly billingService: BillingService) {}
-
-  @Get()
-  getHello(): string {
-    return this.billingService.getHello();
-  }
 }
