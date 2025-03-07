996b47d2ef3b3e6ed78d4585153be56b4e6511b5
fabogit <fabo.dev.email@gmail.com>
2025-03-07 05:06:19 +0100

    #9
    dynamic rmq module

--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,8 +1,66 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 import { RmqService } from './rmq.service';
 
+/**
+ * Interface defining the options for registering the RmqModule.
+ * It specifies the name used to identify the RabbitMQ client.
+ */
+interface RmqModuleOptions {
+  /**
+   * The name of the RabbitMQ client. This name is used to configure and inject the client.
+   */
+  name: string;
+}
+
+/**
+ * Module for providing RabbitMQ client and service functionalities.
+ * This module offers a dynamic registration feature, allowing to create and configure RabbitMQ clients on-demand
+ * based on provided options, primarily used for microservice communication via RabbitMQ.
+ * It exports both the `RmqService` and `ClientsModule` to make RabbitMQ functionalities available to other modules.
+ * @exports {RmqModule} Exports the `RmqModule` class, making it available for import in other modules.
+ */
 @Module({
   providers: [RmqService],
   exports: [RmqService],
 })
-export class RmqModule {}
+export class RmqModule {
+  /**
+   * Registers the `RmqModule` dynamically, creating a configured RabbitMQ client.
+   * This static method allows for dynamic configuration of the RabbitMQ client based on the options provided.
+   * It sets up a `ClientsModule` with asynchronous registration to utilize `ConfigService` for configuration.
+   * @static
+   * @param {RmqModuleOptions} options Options to configure the `RmqModule`, including the client `name`.
+   * @returns {DynamicModule} A dynamically configured NestJS module.
+   */
+  static register({ name }: RmqModuleOptions): DynamicModule {
+    return {
+      module: RmqModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            /**
+             * Factory function to create the RabbitMQ client configuration asynchronously.
+             * It utilizes the `ConfigService` to fetch configuration values for RabbitMQ connection details,
+             * such as the URI and queue name, from the application's configuration.
+             * @param {ConfigService} config The `ConfigService` instance injected for configuration access.
+             * @returns An object containing the `transport` and `options` for the RabbitMQ client.
+             */
+            useFactory: (config: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [config.get<string>('RABBIT_MQ_URI')],
+                queue: config.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      // Export ClientsModule to make the registered client available for injection
+      exports: [ClientsModule],
+    };
+  }
+}
--- libs/common/src/rmq/rmq.service.ts
+++ libs/common/src/rmq/rmq.service.ts
@@ -2,17 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RmqOptions, Transport } from '@nestjs/microservices';
 
+/**
+ * Injectable service for providing RabbitMQ connection options.
+ * This service encapsulates the logic for generating configurations needed to connect to RabbitMQ,
+ * leveraging the `ConfigService` to retrieve necessary connection details from the application's configuration.
+ */
 @Injectable()
 export class RmqService {
+  /**
+   * Constructor for RmqService.
+   * @param {ConfigService} configService - Injected `ConfigService` instance.
+   * The `ConfigService` is used to access application-wide configuration variables, specifically for RabbitMQ settings.
+   */
   constructor(private readonly configService: ConfigService) {}
 
+  /**
+   * Generates and returns RabbitMQ connection options for a given `queue`.
+   * This method constructs an `RmqOptions` object, which is used by NestJS microservices to establish
+   * a connection with RabbitMQ. It retrieves connection URLs and queue names from the `ConfigService`.
+   * @param queue - The name of the RabbitMQ queue for which to generate options. This is used to dynamically determine the queue name from the configuration.
+   * @param [acknowledgements=false] - Optional parameter to determine if message acknowledgements should be disabled (`noAck: true`). Defaults to `false`, meaning acknowledgements are enabled by default.
+   * @returns {RmqOptions} An `RmqOptions` object configured with connection details for RabbitMQ, ready to be used in a NestJS microservice client or listener.
+   */
   getOptions(queue: string, acknowledgements = false): RmqOptions {
     return {
+      // Specifies the transport protocol as RabbitMQ
       transport: Transport.RMQ,
       options: {
+        // Retrieves the RabbitMQ connection URI from the configuration
         urls: [this.configService.get<string>('RABBIT_MQ_URI')],
+        // Dynamically retrieves the queue name from configuration based on the provided queue parameter
         queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
+        // Sets whether message acknowledgements are enabled or disabled. When true, acknowledgements are disabled.
         noAck: acknowledgements,
+        // Configures messages to be persistent, ensuring they survive broker restarts.
         persistent: true,
       },
     };
